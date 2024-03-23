7554d6cab377e8daba2f9d8010830813bccd4cf2
NIYIBIZI Prince <122948625+niyibizimadeit@users.noreply.github.com>
2024-03-23 14:07:28 +0800

    Create numberstowords.py

---
+++ numberstowords.py
@@ -0,0 +1,5 @@
+from num2words import num2words
+print(num2words(36, to = 'ordinal'))
+print(num2words(36, to = 'ordinal_num'))
+print(num2words(36, to = 'year'))
+print(num2words(36, to = 'currency'))
